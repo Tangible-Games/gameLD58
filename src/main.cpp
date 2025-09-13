fa61267fb3e49d1f19765a280d5dd65408fa2ada
Ivan Loskutov <loskutov.ivan@gmail.com>
2025-09-13 19:03:28 +0200

    Add pre-commit clang formating hook

--- src/main.cpp
+++ src/main.cpp
@@ -53,7 +53,6 @@ int main(int /* argc */, char* /* argv */[]) {
 
   SDL_Window* window = SDL_CreateWindow("window", SDL_WINDOWPOS_UNDEFINED,
                                         SDL_WINDOWPOS_UNDEFINED, 480, 272, 0);
-
   SDL_Renderer* renderer = SDL_CreateRenderer(
       window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 
