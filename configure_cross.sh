6ca31d4d48222b5a554987ddf5858c7df2c77219
Ivan Loskutov <loskutov.ivan@gmail.com>
2025-09-25 22:43:18 +0200

    Reconfigure builddir if exist

--- configure_cross.sh
+++ configure_cross.sh
@@ -2,4 +2,5 @@ source ./build_env.sh
 ARGS=$@
 # hack for build on macos
 [[ $OSTYPE == 'darwin'* ]] && ARGS="$ARGS -Db_ndebug=true"
+[[ -d builddir ]] && ARGS="$ARGS --reconfigure"
 meson setup builddir --cross-file crosscompile.txt $ARGS
