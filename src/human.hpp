8b540cc1de70c70682c0ea91966e0cd1055fa8a0
Ivan Loskutov <loskutov.ivan@gmail.com>
2025-10-05 22:47:49 +0200

    human: load configuration

--- src/human.hpp
+++ src/human.hpp
@@ -10,8 +10,41 @@
 
 namespace gameLD58 {
 
+constexpr auto kHumanConfigPath{"assets/human.json"};
+struct HumanConfiguration {
+  static HumanConfiguration load() {
+    std::ifstream file;
+    HumanConfiguration ret;
+
+    file.open(kHumanConfigPath);
+    if (file.is_open()) {
+      // Parse JSON and extract values
+      nlohmann::json config = nlohmann::json::parse(file);
+      file.close();
+
+      ret.accelerationCapturing =
+          config["accelerationY"]["capturing"].get<float>();
+      ret.accelerationFalling = config["accelerationY"]["falling"].get<float>();
+      ret.velocityDeadly = config["velocityY"]["deadly"].get<float>();
+    } else {
+      LOGE("Failed to load {}", kHumanConfigPath);
+    }
+
+    return ret;
+  }
+
+  static HumanConfiguration& configuration() {
+    static HumanConfiguration config{HumanConfiguration::load()};
+    return config;
+  }
+
+  float accelerationCapturing{0};
+  float accelerationFalling{0};
+  float velocityDeadly{0};
+};
+
 struct HumanTexture {
-  static std::shared_ptr<SDL_Texture> get(
+  static std::shared_ptr<SDL_Texture> texture(
       std::shared_ptr<SDL_Renderer> renderer) {
     static std::shared_ptr<SDL_Texture> _ = std::shared_ptr<SDL_Texture>(
         IMG_LoadTexture(renderer.get(), "assets/human.png"),
@@ -27,26 +60,27 @@ class Human {
       : rect(r),
         groundY_(r.center.y),
         renderer_(renderer),
-        texture_(HumanTexture::get(renderer)) {}
+        texture_(HumanTexture::texture(renderer)),
+        configuration_(HumanConfiguration::configuration()) {}
 
-  void Update(float dt) {
-    (void)dt;
+  bool Update(float dt) {
     if (captured_) {
-      acc_.y -= 2000 * dt;
+      acc_.y -= configuration_.accelerationCapturing * dt;
     } else if (rect.center.y < groundY_) {
-      acc_.y += 5000 * dt;
+      acc_.y += configuration_.accelerationFalling * dt;
     }
 
     auto v = acc_ * dt;
     rect.center.y += v.y * dt;
     if (rect.center.y > groundY_) {
-      if (v.y > 30.0) {
-        LOGD("Dead");
-        // TODO
-      }
       rect.center.y = groundY_;
       acc_.y = 0;
+      if (v.y > configuration_.velocityDeadly) {
+        // TODO: Do something
+        return false;
+      }
     }
+    return true;
   }
 
   void DrawTo(const SDL_FRect& r) {
@@ -59,7 +93,7 @@ class Human {
 
   std::shared_ptr<SDL_Renderer> renderer_;
   std::shared_ptr<SDL_Texture> texture_;
-  std::shared_ptr<SDL_Texture> textureCaptured_;
+  HumanConfiguration configuration_;
   Symphony::Math::Vector2d acc_;
   bool captured_{false};
 };
