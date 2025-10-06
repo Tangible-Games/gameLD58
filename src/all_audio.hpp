14dfe7ff306d517d5a4bd26f980618207c77eb37
Dmitrii Tokarev <truvor.skameikin@gmail.com>
2025-10-06 22:49:39 +0200

    ?

--- src/all_audio.hpp
+++ src/all_audio.hpp
@@ -7,6 +7,17 @@ enum class Sound {
   kButtonClick,
   kHumanoidSelect,
   kKaChing,
+  kBeamLoop,
+  kBodyFall_1,
+  kBodyFall_2,
+  kBodyFall_3,
+  kCapture,
+  kPanic_1,
+  kPanic_2,
+  kPanic_3,
+  kPanic_4,
+  kPanic_5,
+  kPanic_5,
 };
 
 struct AllAudio {
@@ -18,10 +29,38 @@ AllAudio LoadAllAudio() {
 
   result.audio[Sound::kButtonClick] = Symphony::Audio::LoadWave(
       "assets/button_click.wav", Symphony::Audio::WaveFile::kModeLoadInMemory);
+
   result.audio[Sound::kHumanoidSelect] = Symphony::Audio::LoadWave(
       "assets/select_human.wav", Symphony::Audio::WaveFile::kModeLoadInMemory);
+
   result.audio[Sound::kKaChing] = Symphony::Audio::LoadWave(
       "assets/sell_human.wav", Symphony::Audio::WaveFile::kModeLoadInMemory);
+
+  result.audio[Sound::kBeamLoop] = Symphony::Audio::LoadWave(
+      "assets/beam.wav", Symphony::Audio::WaveFile::kModeLoadInMemory);
+
+  result.audio[Sound::kBodyFall_1] = Symphony::Audio::LoadWave(
+      "assets/bodyfall_1.wav", Symphony::Audio::WaveFile::kModeLoadInMemory);
+  result.audio[Sound::kBodyFall_2] = Symphony::Audio::LoadWave(
+      "assets/bodyfall_2.wav", Symphony::Audio::WaveFile::kModeLoadInMemory);
+  result.audio[Sound::kBodyFall_3] = Symphony::Audio::LoadWave(
+      "assets/bodyfall_3.wav", Symphony::Audio::WaveFile::kModeLoadInMemory);
+
+  result.audio[Sound::kCapture] = Symphony::Audio::LoadWave(
+      "assets/capture.wav", Symphony::Audio::WaveFile::kModeLoadInMemory);
+
+  result.audio[Sound::kPanic_1] = Symphony::Audio::LoadWave(
+      "assets/panic_1.wav", Symphony::Audio::WaveFile::kModeLoadInMemory);
+  result.audio[Sound::kPanic_2] = Symphony::Audio::LoadWave(
+      "assets/panic_2.wav", Symphony::Audio::WaveFile::kModeLoadInMemory);
+  result.audio[Sound::kPanic_3] = Symphony::Audio::LoadWave(
+      "assets/panic_3.wav", Symphony::Audio::WaveFile::kModeLoadInMemory);
+  result.audio[Sound::kPanic_4] = Symphony::Audio::LoadWave(
+      "assets/panic_4.wav", Symphony::Audio::WaveFile::kModeLoadInMemory);
+  result.audio[Sound::kPanic_5] = Symphony::Audio::LoadWave(
+      "assets/panic_5.wav", Symphony::Audio::WaveFile::kModeLoadInMemory);
+  result.audio[Sound::kPanic_6] = Symphony::Audio::LoadWave(
+      "assets/panic_6.wav", Symphony::Audio::WaveFile::kModeLoadInMemory);
 
   return result;
 }
